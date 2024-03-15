459bd3278301e817dc500cd112d33ba87327072a
Sagar Gautam <unstopable.sagar@gmail.com>
2024-03-15 21:17:29 +0545

    Database changed to mysql

--- ecommerce_app/settings.py
+++ ecommerce_app/settings.py
@@ -85,8 +85,15 @@ WSGI_APPLICATION = 'ecommerce_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_ecommerce_app',
+        'USER': 'root', # database on localhost
+        'PASSWORD':'',
+        'HOST':'localhost', # can also write 127.0.0.1:8000
+        'PORT':'3306',
+        'OPTIONS':{
+            'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
